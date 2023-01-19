2a317444c85302063e750496b840fe0c986a9980
calvinmlee <calvinmlee@uchicago.edu>
2023-01-19 19:38:35 +0000

    Finished ints

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -7,3 +7,8 @@
 #   "Wow, you were born in 1940. You're old!"
 
 p "How old are you?"
+curr_year = 2023
+age = gets.chomp
+age = age.to_i
+birth_year = curr_year - age
+p "Wow, you were born in " + birth_year.to_s + ". You're old!"
